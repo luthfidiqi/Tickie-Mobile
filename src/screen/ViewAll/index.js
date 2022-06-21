980d5b50574b6adc40d4bbf6c94c70149b2bc95c
luthfidiqi <luthfidiqi@gmail.com>
2022-06-22 00:05:23 +0700

    view all movie

--- src/screen/ViewAll/index.js
+++ src/screen/ViewAll/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import {View, TouchableOpacity, ScrollView, Text, Image, Button,TextInput,} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, TouchableOpacity, ScrollView, Text, Image, Button,TextInput, FlatList} from 'react-native';
 import styles from './styles';
+
+import axios from '../../utils/axios';
 
 import CardHome from "../../components/CardHome";
 import Footer from "../../components/Footer";
 
 function ViewAll(props) {
   
+  const handleDetail = () => {
+    props.navigation.navigate('Detail');
+  };
+
+  const handleJoin = () => {
+  };
+
+  const handleViewAll = () => {
+    props.navigation.navigate('List');
+  };
+
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(10);
+
+  useEffect(() => {
+    getDataMovie();
+  }, []);
+
+  const getDataMovie = async () => {
+    try {
+        const result = await axios.get(`movie?page=${page}&limit=9`);
+        setData(result.data.data);
+        setTotalPage(result.data.pagination.totalPage);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ScrollView style={{backgroundColor: 'white', height: '100%',}}>
         <View>
@@ -20,13 +51,22 @@ function ViewAll(props) {
               <Text style={styles.btnDate}>November</Text>
               <Text style={styles.btnDate}>December</Text>
             </ScrollView>
-            <View style={{flexDirection: 'row', marginBottom: 20}}>
-              <CardHome {...props}/>
-              <CardHome {...props}/>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-              <CardHome {...props}/>
-              <CardHome {...props}/>
+            <View>
+              <FlatList
+                data={data}
+                keyExtractor={item => item.id}
+                numColumns={2}
+                renderItem={({item}) => (
+                  <View style={styles.card}>
+                    <Image source={{ uri: `https://res.cloudinary.com/luthfidiqi/image/upload/v1649598083/${item.image}`}} style={{width: 110, height: 170, borderRadius:6}}/>
+                    <Text style={{color:'#14142B', fontSize: 14, marginTop:12,}}>{item.name}</Text>
+                    <Text style={{color:'#A0A3BD', fontSize: 12, marginTop:5, marginBottom:25, maxWidth: 120, textAlign: 'center'}}>{item.category}</Text>
+                    <TouchableOpacity style={{width:'100%'}} onPress={handleDetail}>
+                      <Text style={styles.btnCard}>Details</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+              />
             </View>
           </View>
         </View>
--- src/screen/ViewAll/styles.js
+++ src/screen/ViewAll/styles.js
@@ -21,4 +21,25 @@ export default StyleSheet.create({
     borderRadius: 4,
     marginRight: 10,
   },
+  card: {
+    backgroundColor: 'white', 
+    marginRight: 20,
+    marginBottom: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+    padding:16,
+    borderWidth: 1,
+    borderColor: '#DEDEDE',
+  },
+  btnCard: {
+    backgroundColor: '#FFFFFF', 
+    color:'#5F2EEA', 
+    fontSize: 12,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: '#5F2EEA',
+    borderRadius: 4,
+    width: '100%',
+    textAlign: 'center'
+  },
 });
